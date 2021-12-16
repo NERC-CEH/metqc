7b0b845d2d46592ed8a9e25a7419636abdb037a1
thomaszwagerman <thowag@ceh.ac.uk>
2021-12-16 11:15:41 +0000

    Updated code to be compatible with tidyverse style guide.

--- R/ui.R
+++ R/ui.R
@@ -3,49 +3,68 @@ ui <- dashboardPage(skin = "green",
                     dashboardHeader(title = "Met Data Validation"),
                     dashboardSidebar(
                       sidebarMenu(
-                        menuItem("Dashboard", tabName = "dashboard",icon = icon("dashboard")),
-                        menuItem("Information", tabName = "information",icon = icon("th"))
+                        menuItem("Dashboard", tabName = "dashboard",
+                                 icon = icon("dashboard")),
+                        menuItem("Information", tabName = "information",
+                                 icon = icon("th"))
                       )
                     ),
                     dashboardBody(
                       useShinyjs(),
-                      extendShinyjs(text = jsResetCode, functions = "reset"),
+                      extendShinyjs(text = js_reset_code, functions = "reset"),
                       tabItems(
                         tabItem(tabName = "dashboard",
                                 fluidRow(
-                                  box(title = "Database Controls", status = "success", solidHeader = TRUE,
+                                  box(title = "Database Controls",
+                                      status = "success", solidHeader = TRUE,
                                       helpText("Select your required processing start and end times below."),
                                       column(width = 6,
                                              uiOutput("start_date")),
                                       column(width = 3,
-                                             numericInput("shour", value = 00, label = "Hour", min = 0, max = 23, step = 1)),
+                                             numericInput("shour",
+                                                          value = 00, label = "Hour",
+                                                          min = 0, max = 23, step = 1)),
                                       column(width = 3,
-                                             numericInput("smin", value = 00, label = "Minute", min = 0, max = 59, step = 1)),
+                                             numericInput("smin",
+                                                          value = 00, label = "Minute",
+                                                          min = 0, max = 59, step = 1)),
                                       column(width = 6,
                                              uiOutput("end_date")),
                                       column(width = 3,
-                                             numericInput("ehour", value = 00, label = "Hour", min = 0, max = 23, step = 1)),
+                                             numericInput("ehour",
+                                                          value = 00, label = "Hour",
+                                                          min = 0, max = 23, step = 1)),
                                       column(width = 3,
-                                             numericInput("emin", value = 00, label = "Minute", min = 0, max = 59, step = 1)),
-                                      sliderInput("intslider", label = "Smoothness (number of knots in cr spline):", min = 1, max = 32, value = 10, step = 1),
+                                             numericInput("emin",
+                                                          value = 00, label = "Minute",
+                                                          min = 0, max = 59, step = 1)),
+                                      sliderInput("intslider",
+                                                  label = "Smoothness (number of knots in cr spline):",
+                                                  min = 1, max = 32, value = 10, step = 1),
                                       uiOutput("select_variables"),
                                       actionButton("retrieve_data", "Retrieve from database")
                                   ),
-                                  box(title = "Calendar heatmap plot", status = "success", solidHeader = TRUE,
+                                  box(title = "Calendar heatmap plot",
+                                      status = "success", solidHeader = TRUE,
                                       plotOutput("heatmap_plot")
                                   ),
                                 ),
                                 hidden(
                                   fluidRow(id = "extracted_data",
-                                           box(title = "Plotted Extracted Data", status = "success", solidHeader = TRUE,
+                                           box(title = "Plotted Extracted Data",
+                                               status = "success", solidHeader = TRUE,
                                                tabsetPanel(id = "plotTabs",
                                                            girafeOutput("interactive_plot"),
                                                            type = "tabs"),
-                                               shinyjs::disabled(actionButton("reset", label = "Restart app")),
-                                               shinyjs::disabled(actionButton("delete", label = "Delete selection")),
-                                               shinyjs::disabled(actionButton("nochange", label = "Finished checking variable for date range.")),
+                                               shinyjs::disabled(actionButton("reset",
+                                                                              label = "Restart app")),
+                                               shinyjs::disabled(actionButton("delete",
+                                                                              label = "Delete selection")),
+                                               shinyjs::disabled(actionButton("nochange",
+                                                                              label = "Finished checking variable for date range.")),
                                            ),
-                                           box(title = "Data Change Log", status = "success", solidHeader = TRUE,
+                                           box(title = "Data Change Log",
+                                               status = "success", solidHeader = TRUE,
                                                dataTableOutput("summarytable"),
                                                shinyjs::disabled(actionButton("submitchanges", "Submit changes"))
                                            )
@@ -56,7 +75,5 @@ ui <- dashboardPage(skin = "green",
                                 h2("Information placeholder"),
                                 p("This app provides an interface to the field sites database and allows a user to plot data, remove dubious data and fill gaps with predictions."))
                       )
-                    )
-                    
-                    ,busyIndicator(text = "Please wait...", wait = 500)
+                    ), busy_indicator(text = "Please wait...", wait = 500)
 ) #end of navbar page
